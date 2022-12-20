143d2ebb7c36d9f94a23c3eadff24a1ef4484e59
David Koenig <david.koenig09@outlook.com>
2022-12-20 10:15:54 +0100

    Changed icons

---
+++ pimenu_restart.ps1
@@ -0,0 +1,4 @@
+scp -i C:\Users\david\.ssh\homenet_dave.key $PSScriptRoot\pimenu.yaml dave@192.168.55.130:/home/dave/pimenu/pimenu.yaml
+scp -i C:\Users\david\.ssh\homenet_dave.key $PSScriptRoot\pimenu.sh dave@192.168.55.130:/home/dave/pimenu/pimenu.sh
+ssh dave@192.168.55.130 -i C:\Users\david\.ssh\homenet_dave.key 'kill $(pidof python2)'
+ssh dave@192.168.55.130 -i C:\Users\david\.ssh\homenet_dave.key 'export DISPLAY=:0.0;python2 /home/dave/pimenu/pimenu.py fs'
